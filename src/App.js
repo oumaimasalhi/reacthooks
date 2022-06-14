1dd5e80c083e0c9b92f3a7ee0d604f73ebb089ec
oumaimasalhi <oumaimamaki12@gmail.com>
2022-06-14 13:02:30 +0200

    reacthooks

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MovieCard from '../src/Components/MovieCard/MovieCard'
+function App() {
+  return (
+    <div className="App">
+      <MovieCard/>
+      
+     
+    </div>
+  );
+}
+
+export default App;
